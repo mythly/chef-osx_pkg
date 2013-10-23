5a0da220ba672132a41f4e434fa8347153e952d2
ikrima <contact@ikrima.com>
2013-10-22 18:49:22 -0700

    Update package.rb

--- resources/package.rb
+++ resources/package.rb
@@ -23,6 +23,8 @@ attribute :source,      :kind_of => String
 attribute :checksum,    :kind_of => String, :default => nil
 attribute :package_id,  :kind_of => String, :default => nil
 attribute :installed,   :kind_of => [TrueClass, FalseClass], :default => false
+attribute :user,        :kind_of => String, :default => nil
+attribute :group,       :kind_of => String, :default => nil
 
 def initialize(name, run_context = nil)
   super
