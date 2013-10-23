0f2a4e2b504e3c4f553883ace473157da42d78e4
ikrima <contact@ikrima.com>
2013-10-22 20:57:23 -0700

    Update package.rb

--- providers/package.rb
+++ providers/package.rb
@@ -38,7 +38,7 @@ action :install do
     bash "Executing package #{pkg_file}" do
       #code "sudo su - #{new_resource.user} -c 'sudo /usr/sbin/installer -pkg #{downloaded_file} -target /'"
       #code "sudo su - #{new_resource.user} -i -l -c \"bash -i -l -c 'sudo installer -pkg #{downloaded_file} -target /'\""
-      code "sudo -u #{new_resource.user} -i 'installer -pkg #{downloaded_file} -target /'"
+      code "sudo -u #{new_resource.user} -i 'sudo installer -pkg #{downloaded_file} -target /'"
       user new_resource.user
     end
 	
